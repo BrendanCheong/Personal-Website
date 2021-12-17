4e21242f3255b903e53bc972629a036de1f9773c
BrendanCheong <brendancej82@gmail.com>
2021-12-17 22:15:14 +0800

    small changes to Navbar and Layout styling

--- components/Global/Layout.js
+++ components/Global/Layout.js
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
     return (
         <div className="outline-none">
-            <div className="flex flex-col w-screen h-full min-h-screen transition duration-500 ease-in-out bg-white dark:bg-bluegray-800">
+            <div className="flex flex-col w-screen h-full min-h-screen transition duration-500 ease-in-out bg-white dark:bg-slate-800">
                 <div className="container relative mx-auto">
                     {children}
                 </div>
